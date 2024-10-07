13e8445f4a0558940e724e2e1d29d79a62e7921c
Gyurkovics Dominik <65687471+gydoma@users.noreply.github.com>
2024-10-07 10:29:40 +0200

    meow

---
+++ meowmeow/Program.cs
@@ -0,0 +1,54 @@
+﻿using meowmeow;
+
+List<Versenyzo> versenyzok = new List<Versenyzo>();
+FileStream fs = new FileStream("forras.txt", FileMode.Open);
+StreamReader sr = new StreamReader(fs);
+while (!sr.EndOfStream)
+{
+    Versenyzo v = new Versenyzo(sr.ReadLine());
+    versenyzok.Add(v);
+}
+sr.Close();
+fs.Close();
+
+#region 1. versenyzők száma 'elit' kategóriában
+
+int elitSzam = versenyzok.Count(v => v.Kategoria == "elit");
+Console.WriteLine(elitSzam);
+
+#endregion
+#region női versenyzők átlagéletkora
+
+var noiAtlagkor = versenyzok
+    .Where(v => v.Nem == "n")
+    .Average(v=>v.Eletkor);
+Console.WriteLine(noiAtlagkor);
+
+#endregion
+#region a versenyzők kerékpározással töltött összideje órában. 2tj-ig kerekítve
+
+
+
+#endregion
+#region - átlagos úszási idő elit junior kategóriában
+
+
+
+#endregion
+#region férfi győztes (legrövidebb összidő)
+
+
+
+#endregion
+#region korkategóriánként a versenyt befejezők száma
+
+var korkat = versenyzok
+    .Select(v => v.Kategoria)
+    .Distinct()
+    .Order();
+foreach (var k in korkat)
+{
+    Console.WriteLine($"\t{k}");
+}
+
+#endregion
---
+++ meowmeow/versenyzo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace meowmeow
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public int Szulev { get; set; }
+        public int Rajt { get; set; }
+        public string Nem { get; set; }
+        public string Kategoria { get; set; }
+        public TimeSpan Iuszas { get; set; }
+        public TimeSpan Idepo1 { get; set; }
+        public TimeSpan Ikerek { get; set; }
+        public TimeSpan Idepo2 { get; set; }
+        public TimeSpan Ifutas { get; set; }
+        public int Eletkor => DateTime.Now.Year - Szulev;
+        public override string ToString()
+        {
+            return $"{Nev} ({Eletkor}) {/*(Nem ? "Férfi" : "Nő")*/Nem}: {Kategoria} Kategóriában";
+        }
+        public Versenyzo(string sor)
+        {
+            string[] darabok = sor.Split(';');
+            Nev = darabok[0];
+            Szulev = Convert.ToInt16(darabok[1]);
+            Rajt = Convert.ToInt16(darabok[2]);
+            Nem = darabok[3];
+            Kategoria = darabok[4];
+            Iuszas = TimeSpan.Parse(darabok[5]);
+            Idepo1 = TimeSpan.Parse(darabok[6]);
+            Ikerek = TimeSpan.Parse(darabok[7]);
+            Idepo2 = TimeSpan.Parse(darabok[8]);
+            Ifutas = TimeSpan.Parse(darabok[9]);
+        }
+    }
+
+}
